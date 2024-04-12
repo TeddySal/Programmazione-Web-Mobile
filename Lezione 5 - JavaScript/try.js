ba66a0112465711f02928266e8d3291503daea39
MrFalso <salillari.tedi@gmail.com>
2024-04-12 19:39:17 +0200

    new

---
+++ Lezione 5 - JavaScript/try.js
@@ -0,0 +1,15 @@
+const image_base = 'https://media.themoviedb.org/t/p/w300_and_h450_bestv2';
+let card = document.getElementById('card-film');
+
+for (let i = 0; i < popolari.results.length; i++) {
+    let film = popolari.results[i];
+    let clone = card.cloneNode(true);
+    let image = clone.getElementsByClassName('card-image-top')[0];
+    let button = clone.getElementsByClassName('btn-primary')[0];
+
+    image.src = image_base + film.poster_path;
+    button.href = 'scheda-film.html?id='+film.id;
+
+    clone.classList.remove('d-none');
+    card.after(clone);
+}
---
+++ Lezione 7 - REST e FETCH/req.js
@@ -0,0 +1,53 @@
+var isChanged = false;
+
+function requestFilm(id) {
+    document.getElementById('card').classList.remove('d-none');
+    
+
+    if (isChanged) {
+        document.querySelectorAll('[id=car]').forEach((element) => element.remove());
+        document.querySelectorAll('[id=planet]').forEach((element) => element.remove());
+    }
+   
+    fetch("https://swapi.info/api/films/"+id)
+        .then((res) => res.json())
+        .then((json) => {
+            //console.log(json);
+            document.getElementById('title').innerHTML = json.title;
+            document.getElementById('director').innerHTML = json.director;
+            document.getElementById('opening').innerHTML = json.opening_crawl;
+            document.getElementById('date').innerHTML = json.created;
+
+            for (let i = 0; i < json.characters.length; i++) {
+                fetch(json.characters[i])
+                    .then((res) => res.json())
+                    .then((person) => {
+                        var car = document.getElementById('car-none');
+                        var clone = car.cloneNode(true);
+                        clone.id = "car";
+
+                        clone.getElementsByClassName('btn')[0].innerHTML = person.name;
+
+                        clone.classList.remove('d-none');
+                        car.after(clone);
+                });
+            }
+
+            for (let i = 0; i < json.planets.length; i++) {
+                fetch(json.planets[i])
+                    .then((res) => res.json())
+                    .then((planet) => {
+                        var pla = document.getElementById('planet-none');
+                        var clone = pla.cloneNode(true);
+                        clone.id = "planet";
+
+                        clone.getElementsByClassName('btn')[0].innerHTML = planet.name;
+
+                        clone.classList.remove('d-none');
+                        pla.after(clone);  
+                });
+            }
+        }
+    );
+    isChanged = true;
+}
