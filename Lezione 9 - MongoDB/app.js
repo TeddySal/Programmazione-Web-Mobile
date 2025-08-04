82f76c73bb311fe2ef652a30ebb7cd4296e80fd7
Teddy <85159219+TeddySal@users.noreply.github.com>
2025-08-04 15:04:22 +0200

    Update app.js

--- Lezione 9 - MongoDB/app.js
+++ Lezione 9 - MongoDB/app.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 const fn = require('./fn');
 
-const uri = "mongodb+srv://teddy:te3br30lo10@fisrtcluster.bkj0sed.mongodb.net/?retryWrites=true&w=majority&appName=FisrtCluster";
+const uri = "mongodb+srv://@fisrtcluster.bkj0sed.mongodb.net/?retryWrites=true&w=majority&appName=FisrtCluster";
 
 const client = new MongoClient(uri);
 
